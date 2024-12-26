aa65a491e7e90208fe67b3fb9ac7e48e1842764d
amitby98 <amitby98@gmail.com>
2024-12-26 17:27:02 +0200

    added country item

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React from "react";
 
-const Country = () => {
+const Country = ({ country }) => {
   return (
-    // TODO: Country component
-    <div>Country</div>
-  )
-}
+    <>
+      <a href='#' className='country scale-effect' data-country-name={country.name}>
+        <div className='country-flag'>
+          <img src={country.flag} alt={country.name} />
+        </div>
+        <div className='country-info'>
+          <h2 className='country-title'>{country.name}</h2>
+          <ul className='country-brief'>
+            <li>
+              <strong>Population:</strong> {country.population}
+            </li>
+            <li>
+              <strong>Region:</strong> {country.region}
+            </li>
+            <li>
+              <strong>Capital:</strong> {country.capital}
+            </li>
+          </ul>
+        </div>
+      </a>
+    </>
+  );
+};
 
-export default Country
+export default Country;
