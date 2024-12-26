90e88a5213e60aa26a72ad841fec63b98a546b30
amitby98 <amitby98@gmail.com>
2024-12-26 18:46:36 +0200

    added dark mode function

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Header = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add("dark-theme");
+    } else {
+      document.body.classList.remove("dark-theme");
+    }
+  }, [darkMode]);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
     <header className='header'>
       <div className='container flex flex-jc-sb flex-ai-c'>
@@ -9,9 +23,9 @@ const Header = () => {
             <h1>Where in the world?</h1>
           </a>
         </div>
-        <button type='button' aria-label='Theme Switcher Button' id='dark-mode-button' className='theme-toggle flex flex-jc-sb flex-ai-c'>
-          <i className='fa-regular fa-moon theme-icon'></i>
-          <span className='theme-text'>Dark Mode</span>
+        <button type='button' aria-label='Theme Switcher Button' id='dark-mode-button' className='theme-toggle flex flex-jc-sb flex-ai-c' onClick={toggleDarkMode}>
+          <i className={`fa-regular ${darkMode ? "fa-sun" : "fa-moon"} theme-icon`}></i>
+          <span className='theme-text'>{darkMode ? "Light Mode" : "Dark Mode"}</span>
         </button>
       </div>
     </header>
