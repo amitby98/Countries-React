37a34d4890e5b361edbcf2533e3b7aa3cc71f8bf
amitby98 <amitby98@gmail.com>
2024-12-26 17:27:20 +0200

    added home elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import "../src/assets/css/common.css";
-import "../src/assets/css/details.css";
-import "../src/assets/css/main.css";
-import "../src/assets/scss/common.scss";
+import "./assets/css/main.css";
+import "./assets/css/details.css";
+import "./assets/css/common.css";
+// import "../src/assets/scss/common.scss";
+// Google Fonts Library
+
+// <link rel="preconnect" href="https://fonts.googleapis.com" />
+// <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+// <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600;800&display=swap" rel="stylesheet" />
+// Fontawesome Library
+
+/* <link rel='stylesheet' href='https://site-assets.fontawesome.com/releases/v6.0.0/css/all.css' />; */
+
 import Home from "./pages/Home";
 
 function App() {
-    return (
-        <>
-            <Home />
-        </>
-    );
+  return (
+    <>
+      <Home />
+    </>
+  );
 }
 
 export default App;
